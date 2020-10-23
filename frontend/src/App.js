192a4b505f3cd1c7e4e05d02998a69c3787ee32b
Mohit <mohit.padhye2011@gmail.com>
2020-10-23 13:28:53 +0545

    Home Product Listing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
-import { Container, Navbar } from 'react-bootstrap'
+import { Container } from 'react-bootstrap'
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 import Header from './components/Header';
 
 const App = () => {
   return (
     <>
-    <Header/>
-    <main className='py-3'>
-    <Container>
-    
-    <h1>Welcome To ProShop</h1>
-    </Container>
-    </main>
-    <Footer/>
+      <Header />
+      <main className='py-3'>
+       <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer/>
     </>
-  );
+  )
 }
 
 export default App;
