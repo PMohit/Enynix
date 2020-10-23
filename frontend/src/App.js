2fbc63dad9d67a70ba69a55cb151f6315e78e545
Mohit <mohit.padhye2011@gmail.com>
2020-10-23 13:07:21 +0545

    React Header | Footer | Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
-
-
+import { Container, Navbar } from 'react-bootstrap'
+import Footer from './components/Footer';
+import Header from './components/Header';
 
 const App = () => {
   return (
     <>
+    <Header/>
+    <main className='py-3'>
+    <Container>
+    
     <h1>Welcome To ProShop</h1>
+    </Container>
+    </main>
+    <Footer/>
     </>
   );
 }
